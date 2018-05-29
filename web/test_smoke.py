ba2a9104202f83dbfa79a061892aa55b39b6a8a6
yatakoi <allkall@yandex.ru>
2018-05-29 10:13:39 +1000

    Initial commit

---
+++ web/test_smoke.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+from main import app
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
